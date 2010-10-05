a851c7e04bb1215f4eabd9f9ad0b0c8ec3a92346
cedricbonhomme <devnull@localhost>
2010-10-05 19:17:16 +0200

    Added GPL v3 preambule.

--- steganalysis-statistics.py
+++ steganalysis-statistics.py
@@ -1,6 +1,24 @@
 #! /usr/local/bin/python
 #-*- coding: utf-8 -*-
 
+# Stéganô - Stéganô is a basic Python Steganography module.
+# Copyright (C) 2010  Cédric Bonhomme - http://cedric.bonhomme.free.fr/
+#
+# For more information : http://bitbucket.org/cedricbonhomme/stegano/
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 __author__ = "Cedric Bonhomme"
 __version__ = "$Revision: 0.1 $"
 __date__ = "$Date: 2010/10/01 $"
