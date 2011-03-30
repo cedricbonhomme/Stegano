a02289a679bf5c02da97a0fc0ecb30cae8d1c35d
cedricbonhomme <devnull@localhost>
2011-03-30 21:42:16 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+ #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+import os
+
+setup(
+    name='Stegano',
+    version='0.2',
+    author='Cédric Bonhomme',
+    author_email='kimble.mandel@gmail.com',
+    packages=['stegano'],
+    #scripts=[''],
+    url='http://bitbucket.org/cedricbonhomme/stegano',
+    platforms = ['Linux'],
+    license='COPYING',
+    description='A Python Steganography module.',
+)
