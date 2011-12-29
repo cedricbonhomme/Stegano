67bdb74bcdbe11b10e6705a84a62eafb157a5bfe
cedricbonhomme <devnull@localhost>
2011-12-29 09:58:01 +0100

    Added example2.sh

---
+++ examples/example2.sh
@@ -0,0 +1,22 @@
+clear
+
+echo  "We're going to test a little Stéganô..."
+
+echo "Hide the message  with the Sieve of Eratosthenes..."
+slsb-set --hide -i ./pictures/Montenach.png -o ./surprise.png --generator eratosthenes -m 'Joyeux Noël!'
+echo ""
+
+echo "Try to reveal with Mersenne numbers..."
+slsb-set --reveal --generator mersenne -i ./surprise.png
+echo ""
+
+echo "Try to reveal with fermat numbers..."
+slsb-set --reveal --generator fermat -i ./surprise.png
+echo ""
+
+echo "Try to reveal with carmichael numbers..."
+slsb-set --reveal --generator carmichael -i ./surprise.png
+echo ""
+
+echo "Try to reveal with Sieve of Eratosthenes..."
+slsb-set --reveal --generator eratosthenes -i ./surprise.png
