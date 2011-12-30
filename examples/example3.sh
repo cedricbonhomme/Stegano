98fe828ffb39d0551bfe8f75edac106e7b9fa0b1
cedricbonhomme <devnull@localhost>
2011-12-30 19:55:21 +0100

    Added comments in example3.sh

--- examples/example3.sh
+++ examples/example3.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
+# Some tests of the LSB method which uses sets (slsb-set). Sets are used in order to select the pixels where the
+# message will be hidden.
 
+
+# Hide the message - LSB with a set defined by the identity function (f(x) = x).
 slsb-set --hide -i examples/pictures/Montenach.png -o ~/enc-identity.png --generator identity -m 'I like steganography.'
 
+# Hide the message - LSB only.
 slsb --hide -i examples/pictures/Montenach.png -o ~/enc.png -m 'I like steganography.'
 
 
-
+# Check if the two generated files are the same.
 sha1sum ~/enc-identity.png ~/enc.png 
 
+
+# The output of slsb is given to slsb-set.
 slsb-set --reveal -i ~/enc.png --generator identity 
 
+# The output of slsb-set is given to slsb.
 slsb --reveal -i ~/enc-identity.png
