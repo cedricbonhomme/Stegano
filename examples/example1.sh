c14476e0737758af15390ddd95889fa1b7b69d33
Cédric Bonhomme <kimble.mandel@gmail.com>
2012-12-16 01:40:35 +0100

    Udated example1.sh.

--- examples/example1.sh
+++ examples/example1.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-#hg clone http://bitbucket.org/cedricbonhomme/stegano/
-#cd stegano
-cd ..
 wget http://www.gnu.org/music/free-software-song.ogg
 slsb --hide -i ./pictures/Montenach.png -o ./pictures/Montenach_enc.png -f ./free-software-song.ogg
 rm free-software-song.ogg
