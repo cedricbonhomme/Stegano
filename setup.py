75b41a2e2e6cdd7ab2e10a1f8bed1a1ca81966c7
Cédric Bonhomme <cedric@cedricbonhomme.org>
2016-04-16 14:15:32 +0200

    Bumped version number

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open('CHANGELOG.rst', 'r') as f:
 
 setup(
     name='Stegano',
-    version='0.5',
+    version='0.5.1',
     author='Cédric Bonhomme',
     author_email='cedric@cedricbonhomme.org',
     packages=packages,
