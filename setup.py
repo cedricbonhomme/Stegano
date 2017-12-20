5ec1f338b264ecafba7374377bc7b12a0f65bb40
Cédric Bonhomme <cedric@cedricbonhomme.org>
2017-12-20 22:17:03 +0100

    Fixed a bug with the new 'encoding' function when using Stegano as a command line tool. No default value was set. Default value is UTF-8.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open('CHANGELOG.rst', 'r') as f:
 
 setup(
     name='Stegano',
-    version='0.8.1',
+    version='0.8.2',
     author='Cédric Bonhomme',
     author_email='cedric@cedricbonhomme.org',
     packages=packages,
