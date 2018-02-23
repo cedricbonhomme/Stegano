894a361340b120f0258e7688132c66b1ae625a13
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-02-23 15:39:48 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -49,8 +49,6 @@ setup(
         'Intended Audience :: Science/Research',
         'Topic :: Security',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
     ]
