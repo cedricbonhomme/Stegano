e9605589b61540422820cf2ab0606d3670dcada7
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-12-22 10:46:33 +0100

    Added mising command line tools.

---
+++ bin/parity.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Stegano - Stegano is a pure Python steganography module.
+# Copyright (C) 2010-2019 Cédric Bonhomme - https://www.cedricbonhomme.org
+#
+# For more information : https://github.com/cedricbonhomme/Stegano
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+__author__ = "Cedric Bonhomme"
+__version__ = "$Revision: 0.7 $"
+__date__ = "$Date: 2016/08/25 $"
+__license__ = "GPLv3"
+
+from PIL import Image
+
+import argparse
+
+try:
+    from stegano.steganalysis import parity
+except:
+    print("Install Stegano: pipx install Stegano")
+
+def main():
+    parser = argparse.ArgumentParser(prog='stegano-steganalysis-parity')
+    parser.add_argument("-i", "--input", dest="input_image_file",
+                    required=True, help="Input image file.")
+    parser.add_argument("-o", "--output", dest="output_image_file",
+                    required=True, help="Output image file.")
+    arguments = parser.parse_args()
+
+    input_image_file = Image.open(arguments.input_image_file)
+    output_image = parity.steganalyse(input_image_file)
+    output_image.save(arguments.output_image_file)
---
+++ bin/statistics.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Stegano - Stegano is a pure Python steganography module.
+# Copyright (C) 2010-2019 Cédric Bonhomme - https://www.cedricbonhomme.org
+#
+# For more information : https://github.com/cedricbonhomme/Stegano
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+__author__ = "Cédric Bonhomme"
+__version__ = "$Revision: 0.1 $"
+__date__ = "$Date: 2016/08/26 $"
+__revision__ = "$Date: 2016/08/26 $"
+__license__ = "GPLv3"
+
+from PIL import Image
+
+import argparse
+
+try:
+    from stegano.steganalysis import statistics
+except:
+    print("Install Stegano: sudo pip install Stegano")
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='stegano-steganalysis-parity')
+    parser.add_argument("-i", "--input", dest="input_image_file",
+                    help="Image file")
+    parser.add_argument("-o", "--output", dest="output_image_file",
+                    help="Image file")
+    arguments = parser.parse_args()
+
+    input_image_file = Image.open(arguments.input_image_file)
+    output_image = statistics.steganalyse(input_image_file)
+    output_image.save(arguments.output_image_file)
